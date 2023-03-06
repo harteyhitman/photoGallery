98524be90e5f34e8b65b61ad8389b6035b077b08
Harteyhitman <oyegoke.a18@gmail.com>
2023-03-06 15:16:32 +0100

    Updates

---
+++ precache-manifest.7e0d1c70b7afef2cb26f2e401407c7aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "be55950a7c80b7f87a7f",
+    "url": "/harteyhitman/photoGallery/static/css/main.0fe509c7.chunk.css"
+  },
+  {
+    "revision": "be55950a7c80b7f87a7f",
+    "url": "/harteyhitman/photoGallery/static/js/main.be55950a.chunk.js"
+  },
+  {
+    "revision": "8654e7e83f475fb519e8",
+    "url": "/harteyhitman/photoGallery/static/js/1.8654e7e8.chunk.js"
+  },
+  {
+    "revision": "0757ad675129f59f3748",
+    "url": "/harteyhitman/photoGallery/static/js/runtime~main.0757ad67.js"
+  },
+  {
+    "revision": "2fdf531369f860893c248f9abcedc85f",
+    "url": "/harteyhitman/photoGallery/index.html"
+  }
+];
