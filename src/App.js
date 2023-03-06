1e87aa3b01b74415d49eb18ee7f1a4cddd7ed91d
Harteyhitman <oyegoke.a18@gmail.com>
2023-03-06 11:20:33 +0100

    nice gallery

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { photos } from "./Photos";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="digital-img">
+      <div className="galleria">
+        <h1>galleria</h1>
+        <p>START SLIDESHOW</p>
+      </div>
+      <div className="photo-gallery">
+      {photos.map((photo) => (
+        <div key={photo.id} className="photo">
+          <img src={photo.imgs} alt={photo.caption} />
+          <div className="description">
+          <h1 className="caption">{photo.caption}</h1>
+          <p className="text">{photo.text}</p></div>
+        </div>
+      ))}
+    </div>
     </div>
   );
 }
---
+++ src/Photos.js
@@ -0,0 +1,94 @@
+export const photos = [
+    {
+        id: 1,
+        imgs: 'https://images.unsplash.com/photo-1677487822570-cf276c25864c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDh8Q0R3dXdYSkFiRXd8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+        caption: "Mohammed Kabir",
+        text: "Senior Developer"
+    },
+    {
+        id: 2,
+        imgs: 'https://images.unsplash.com/photo-1677422516588-9bbdcfd1a31b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDV8Q0R3dXdYSkFiRXd8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+        caption: "Azeez Tosin",
+        text: "Deputy Chief"
+    },
+    {
+        id: 3,
+        imgs: 'https://images.unsplash.com/photo-1677402500191-3a56b77c0339?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDEwfENEd3V3WEpBYkV3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        caption: "Mohammed Abdulsalam",
+        text: "Junior Devops"
+    },
+    {
+        id: 4,
+        imgs: 'https://images.unsplash.com/photo-1677252558828-b81ff3935967?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIxfENEd3V3WEpBYkV3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        caption: "Oyegoke Azeez Sketchers",
+        text: "Student"
+    },
+    {
+        id: 5,
+        imgs: 'https://images.unsplash.com/photo-1677249490921-3246e81f19e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE2fENEd3V3WEpBYkV3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        caption: "Azeez Akman Makanaki ",
+        text: "Pro active Student"
+    },
+    {
+        id: 6,
+        imgs: 'https://images.unsplash.com/photo-1677157371089-7beb6db965c7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM1fENEd3V3WEpBYkV3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        caption: "Seun Ashorin",
+        text: "Prospect Student"
+    },
+    {
+        id: 7,
+        imgs: 'https://plus.unsplash.com/premium_photo-1676637656277-498f73258bec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8Q0R3dXdYSkFiRXd8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
+        caption: "Shubham Dhage",
+        text: "Beautiful Images"
+    },
+    {
+        id: 8,
+        imgs: 'https://plus.unsplash.com/premium_photo-1677228571709-519f0d5153f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE3fENEd3V3WEpBYkV3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        caption: "Shubham Dhage",
+        text: "Beautiful Images"
+    },
+    {
+        id: 9,
+        imgs: 'https://plus.unsplash.com/premium_photo-1677018816184-724e89485e9c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDI1fENEd3V3WEpBYkV3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        caption: "Shubham Dhage",
+        text: "Beautiful Images"
+    },
+    {
+        id: 10,
+        imgs: 'https://images.unsplash.com/file-1661973760703-91f50df21c28image?dpr=2&auto=format&fit=crop&w=416&q=60',
+        caption: "Shubham Dhage",
+        text: "Beautiful Images"
+    },
+    {
+        id: 11,
+        imgs: 'https://plus.unsplash.com/premium_photo-1676637656166-cb7b3a43b81a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDU2fENEd3V3WEpBYkV3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        caption: "Shubham Dhage",
+        text: "Beautiful Images"
+    },
+    {
+        id: 12,
+        imgs: 'https://images.unsplash.com/photo-1676650591958-058e0b34eac7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDU3fENEd3V3WEpBYkV3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        caption: "Shubham Dhage",
+        text: "Beautiful Images"
+    },
+    {
+        id: 13,
+        imgs: 'https://plus.unsplash.com/premium_photo-1677143320070-1acc2cf476e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDc1fENEd3V3WEpBYkV3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        caption: "Shubham Dhage",
+        text: "Beautiful Images"
+    },
+    {
+        id: 14,
+        imgs: 'https://plus.unsplash.com/premium_photo-1676885447012-c17dc862434f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDc4fENEd3V3WEpBYkV3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        caption: "Shubham Dhage",
+        text: "Beautiful Images"
+    },
+    {
+        id: 15,
+        imgs: 'https://images.unsplash.com/photo-1676985086410-243e288378a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE5fGlVSXNuVnRqQjBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+        caption: "Shubham Dhage",
+        text: "Beautiful Images"
+    }
+   
+]
+
