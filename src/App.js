5e58e89af5bfaa510623b76624e4511026538bd3
Harteyhitman <oyegoke.a18@gmail.com>
2023-03-06 11:49:07 +0100

    photo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React from "react";
 import "./App.css";
-// import { photos } from "./Photos";
+import { photos } from "./Photos";
 
 
 
 function App() {
   return (
-    <h1>hello</h1>
-    // <div className="digital-img">
-    //   <div className="galleria">
-    //     <h1>galleria</h1>
-    //     <p>START SLIDESHOW</p>
-    //   </div>
-    //   <div className="photo-gallery">
-    //   {photos.map((photo) => (
-    //     <div key={photo.id} className="photo">
-    //       <img src={photo.imgs} alt={photo.caption} />
-    //       <div className="description">
-    //       <h1 className="caption">{photo.caption}</h1>
-    //       <p className="text">{photo.text}</p></div>
-    //     </div>
-    //   ))}
-    // </div>
-    // </div>
+    <div className="digital-img">
+      <div className="galleria">
+        <h1>galleria</h1>
+        <p>START SLIDESHOW</p>
+      </div>
+      <div className="photo-gallery">
+      {photos.map((photo) => (
+        <div key={photo.id} className="photo">
+          <img src={photo.imgs} alt={photo.caption} />
+          <div className="description">
+          <h1 className="caption">{photo.caption}</h1>
+          <p className="text">{photo.text}</p></div>
+        </div>
+      ))}
+    </div>
+    </div>
   
   );
 }
